929748f73a96c804c5deabf19f3f2d9f56096bb3
多賀谷直樹 <tagayanaoki@tagayanaokinoMacBook-Pro.local>
2021-05-12 01:04:39 +0900

    Rails課題1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'schedules/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'posts#top' #トップページ表示
 
-  resources :posts # postsのCRUD処理
+  resources :schedules # schedulesのCRUD処理
 end
